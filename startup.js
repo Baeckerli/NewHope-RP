ab5f6a4ca90f4b7e07be48ee7aa4ec6307b8c4fd
Baeckerli <83593313+Baeckerli@users.noreply.github.com>
2021-05-03 16:29:00 +0200

    Updated version
    Added Server Files

--- startup.js
+++ startup.js
@@ -15,31 +15,37 @@ function handleConnect(player){
 alt.on('playerDeath', handleDeath);
 
 export const DeadPlayers = {};
-const TimeBetweenRespawn = 5000; 
+const TimeBetweenRespawn = 5000; // 5 Seconds
 
-
+/**
+ * @param {alt.Player} player
+ */
 function handleDeath(player) {
     if (deadPlayers[player.id]) {
         return;
     }
 
     deadPlayers[player.id] = alt.setTimeout(() => {
+        // Check if the player still has an entry.
         if (deadPlayers[player.id]) {
             delete deadPlayers[player.id];
         }
 
+        // Check if the player hasn't just left the server yet.
         if (!player || !player.valid) {
             return;
         }
 
-        player.spawn(0, 0, 0, 0);
+        player.spawn(0, 0, 0, 0); // Respawn the player.
     }, TimeBetweenRespawn);
 }
 export function cancelRespawn(player) {
+    // Check if an entry exists for a player respawn.
     if (!deadPlayers[player.id]) {
         return;
     }
 
+    // This cancels the timeout from ever being finished.
     alt.clearTimeout(deadPlayers[player.id]);
     delete deadPlayers[player.id];
 }
