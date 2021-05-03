f7a7a8052cdfa2c9f87c2784d1274c0e40f809d4
Baeckerli <83593313+Baeckerli@users.noreply.github.com>
2021-05-03 16:25:28 +0200

    fixes
    Spawn fixes

--- startup.js
+++ startup.js
@@ -15,37 +15,31 @@ function handleConnect(player){
 alt.on('playerDeath', handleDeath);
 
 export const DeadPlayers = {};
-const TimeBetweenRespawn = 5000; // 5 Seconds
+const TimeBetweenRespawn = 5000; 
 
-/**
- * @param {alt.Player} player
- */
+
 function handleDeath(player) {
     if (deadPlayers[player.id]) {
         return;
     }
 
     deadPlayers[player.id] = alt.setTimeout(() => {
-        // Check if the player still has an entry.
         if (deadPlayers[player.id]) {
             delete deadPlayers[player.id];
         }
 
-        // Check if the player hasn't just left the server yet.
         if (!player || !player.valid) {
             return;
         }
 
-        player.spawn(0, 0, 0, 0); // Respawn the player.
+        player.spawn(0, 0, 0, 0);
     }, TimeBetweenRespawn);
 }
 export function cancelRespawn(player) {
-    // Check if an entry exists for a player respawn.
     if (!deadPlayers[player.id]) {
         return;
     }
 
-    // This cancels the timeout from ever being finished.
     alt.clearTimeout(deadPlayers[player.id]);
     delete deadPlayers[player.id];
 }
